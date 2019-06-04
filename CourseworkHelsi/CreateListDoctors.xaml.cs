958100c22263c71f537f7f8a7735d72bb76133f8
Elena Ilyshun <lenailyshun@gmail.com>
2019-06-04 11:02:26 +0300

    Add class DoctorsService

--- CourseworkHelsi/CreateListDoctors.xaml.cs
+++ CourseworkHelsi/CreateListDoctors.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Bogus;
+using System;
+using System.ComponentModel;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace CourseworkHelsi
 {
@@ -35,7 +26,7 @@ namespace CourseworkHelsi
             Seed(con);
             con.Close();
         }
-      
+
 
         private void GenerateTabels(SQLiteConnection con)
         {
@@ -68,7 +59,77 @@ namespace CourseworkHelsi
             cmd.ExecuteNonQuery();
             #endregion
 
+            var userFaker = new Faker<DoctorsService>("uk")
+                .RuleFor(o => o.Name, f => f.Name.FirstName())
+                .RuleFor(o => o.LastName, f => f.Name.LastName())
+       
            
+                .RuleFor(o => o.Birthday, f => f.Date.Past());
+            var list = userFaker.Generate(1000);
+            //foreach (var user in list)
+            //{
+            //    string nameGroup = user.Name;
+            //    int id = user.Id;
+            //    string lastName = user.LastName;
+            //    int age = user.Age;
+            //    int birthday = 1990;
+            //    string city = user.City;
+            //    string query = $"INSERT INTO USERS (NAME, LASTNAME, CITY, AGE, BIRTHDAY) VALUES ('{nameGroup}','{lastName}','{city}', {age},{birthday})";
+            //    SQLiteCommand cmd = new SQLiteCommand(query, con);
+            //    cmd.ExecuteNonQuery();
+            //    cmd.Cancel();
+            //}
+        }
+    }
+    public class DoctorsService : INotifyPropertyChanged
+    {
+
+        private string name;
+        private string lastname;
+        private DateTime birthday;
+        public DateTime Birthday
+        {
+            get { return this.birthday; }
+            set
+            {
+                if (this.birthday != value)
+                {
+                    this.birthday = value;
+                    this.NotifyPropertyChanged("Birthday");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastname; }
+            set
+            {
+                if (this.lastname != value)
+                {
+                    this.lastname = value;
+                    this.NotifyPropertyChanged("LastName");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
         }
     }
 }
