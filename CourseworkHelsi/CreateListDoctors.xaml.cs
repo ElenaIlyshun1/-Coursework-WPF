6bec990141b66b7bebbde35b27ce2c109bbbeaae
Elena Ilyshun <lenailyshun@gmail.com>
2019-06-04 10:06:26 +0300

    Install SQlite

---
+++ CourseworkHelsi/CreateListDoctors.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseworkHelsi
+{
+    /// <summary>
+    /// Interaction logic for CreateListDoctors.xaml
+    /// </summary>
+    public partial class CreateListDoctors : Window
+    {
+        private string tblNameDoctors = "tblNameDoctors";
+        public CreateListDoctors()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreateNameDoctors_Click(object sender, RoutedEventArgs e)
+        {
+            string dbName = txtNameBD.Text;
+            SQLiteConnection con = new SQLiteConnection($"Data Source={dbName}");
+            con.Open();
+            GenerateTabels(con);
+            Seed(con);
+            con.Close();
+        }
+    }
+}
