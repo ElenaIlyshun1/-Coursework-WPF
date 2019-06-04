70bd3e92b804789b7d830ac340f046ae5b52a2d1
maksutroman <maksutroman@gmail.com>
2019-06-04 11:02:47 +0300

    Add window City

---
+++ CourseworkHelsi/CityWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseworkHelsi
+{
+    /// <summary>
+    /// Interaction logic for CityWindow.xaml
+    /// </summary>
+    public partial class CityWindow : Window
+    {
+        private string city = "city";
+        public CityWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateTabelForCity(SQLiteConnection con)
+        {
+            string query = $"CREATE TABLE IF NOT EXISTS {city} " +
+                                "(Id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                    "City VARCHAR NOT NULL " +
+                                ");";
+            SQLiteCommand cmd = new SQLiteCommand(query, con);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
